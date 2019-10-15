4a864527f892c78a6ec3c2baca6448fb4708735f
Aaron Christiansen <aaronc20000@gmail.com>
2019-10-15 22:53:33 +0100

    Alter BuildCommand to use APIs

--- src/cli/build_command.rb
+++ src/cli/build_command.rb
@@ -3,10 +3,10 @@ using Rainbow
 
 module HSE
   module CLI
-    BuildCommand = Command.new('build', 'Build a Markdown email to HTML', '(input | \'latest\') [output]', 0..2) do    
-      input_filepath = arg_to_filepath(ARGV[1])
+    BuildCommand = Command.new('build', 'Build a Markdown email to HTML', '(input | \'latest\') [output]', 0..2) do |input, output|
+      input_filepath = arg_to_filepath(input)
       content = HSE::Templating::Document.from_text(File.read(input_filepath)).render
-      output_filepath = ARGV[2] || "#{input_filepath.gsub(/(.*)\.([^\/]+)$/, '\1')}.html" # cursed regex to remove file extension
+      output_filepath = output || arg_to_filepath(input, :html) # cursed regex to remove file extension
       File.write(output_filepath, content)
 
       puts "Built email ".green + input_filepath + " to ".green + output_filepath
